ae2f900e65da49b9839de545c301a66f6791fa97
Brendan Schacht <schacht.brendan@gmail.com>
2022-11-10 00:16:31 -0600

    adds styles and blocks for the file containers

--- blackboard.js
+++ blackboard.js
@@ -3,4 +3,23 @@ class Block {
         this.name = name;
         this.path = path;
     }
+    deleteBlock(){
+        let index = proj.blocks.indexOf(this);
+        proj.blocks.splice(index, 1);
+    }
+}
+
+/**
+ * Makes a div slowly disappear and then removes it from the DOM
+ */
+function fadeOutAndRemove(element){
+    let opacity = 1;
+    let interval = setInterval(function(){
+        if (opacity <= 0.1){
+            clearInterval(interval);
+            element.remove();
+        }
+        element.style.opacity = opacity;
+        opacity -= opacity * 0.1;
+    }, 20);
 }
