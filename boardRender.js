3612a40fcf09521dfd106d3ea30fac00de80e4da
Brendan Schacht <schacht.brendan@gmail.com>
2022-11-23 11:23:04 -0600

    Adds rudimentary popup framework

--- boardRender.js
+++ boardRender.js
@@ -1,3 +1,22 @@
 function getHtmlForBlock(blockId){
     let html = "";
+    let block = proj.blocks[blockId];
+
+    html += '<div class="boardBlockContainer">';
+    html += '<img class="boardBlockImg" id="bbItem1" src="img/placeholder.png">';
+    html += '<div class="boardBlock">' + block.name + '</div>';
+    html += '</div>';
+
+    return html;
 }
+
+function renderBlocks(){
+    let html = "";
+    let i = 0;
+    proj.blocks.forEach(element => {
+        html += getHtmlForBlock(i++);
+    });
+    document.getElementById("blockZone").innerHTML = html;
+}
+
+renderBlocks();
